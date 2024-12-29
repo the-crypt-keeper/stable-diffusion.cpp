11f0b68350fcedc1d567fd36b36d4a44496518a9
Stéphane du Hamel <stephduh@live.fr>
2024-12-29 22:01:36 +0100

    Fix compile

--- stable-diffusion.cpp
+++ stable-diffusion.cpp
@@ -1733,6 +1733,7 @@ sd_image_t* txt2img(sd_ctx_t* sd_ctx,
                                                slg_scale,
                                                skip_layer_start,
                                                skip_layer_end,
+                                               NULL,
                                                preview_mode,
                                                preview_interval,
                                                step_callback);
@@ -2023,7 +2024,7 @@ SD_API sd_image_t* img2vid(sd_ctx_t* sd_ctx,
                                                  -1,
                                                  SDCondition(NULL, NULL, NULL),
                                                  {},
-                                                 0, 0, 0,
+                                                 0, 0, 0, NULL,
                                                  (sd_preview_policy_t)0, 1,
                                                  NULL);
 
